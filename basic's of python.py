8dbcc82069b94c9e4304ff4553f1e98eccb6c230
benjaminofori235-maker <benjaminofori235@gmail.com>
2025-12-10 16:46:31 +0000

    Create basic's of python.py with music lists
    Initialize a Python script with a playlist and top artists, demonstrating list operations.

---
+++ basic's of python.py
@@ -0,0 +1,19 @@
+Play_list =["Time Flies","Fein"," All my life","Solo","Make it to the morning"]
+#print the list
+print(Play_list)
+print(len(Play_list)) # to check the lenghth of the list
+
+
+top_artist = ["Drake","PND","J cole","Future","Travis scott"]
+
+my_music = [Play_list,top_artist]
+print(my_music)
+print(len(my_music))
+
+print(my_music[0:3]) #asscessing "All my life" from playlist
+
+Play_list.insert(0,"perfect")
+print(Play_list)
+
+Play_list.append("Blinding lights")
+print(Play_list) 
