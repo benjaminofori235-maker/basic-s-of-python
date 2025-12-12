f60203b82fef17de0c47d3bda475a9bf6314c58e
benjaminofori235-maker <benjaminofori235@gmail.com>
2025-12-12 15:00:08 +0000

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,18 @@
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print("You are eligible to watch any movie including Aldut rated.")
+#if age < 13
+# Print("You can watch G and PG-13 rated movies")
+# Else if age>= 13 and age <18:
+#     print("You can watch PG-13 and lower rated movies.")
+#if age < 7
+# print("You can watch G-rated movies only.")
+# Else if age >=7 and age <13:
+#     print("You can watch G and PG-rated  movies.")
+# If age < 7
+# Print "You should watch only G-rated (General Audience) movies."
+# Else if age >= 7 and age < 13
+# Print "You can watch G and PG-rated movies."
+# Else if age >= 13 and age < 18
+# Print "You can watch PG-13 and G movies."
+
